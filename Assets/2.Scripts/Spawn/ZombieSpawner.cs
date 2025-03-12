28f451e64cf05f5b0f87cef783c90314e21e9278
ckd0dn <74403510+ckd0dn@users.noreply.github.com>
2025-03-12 21:33:33 +0900

    Update ZombieSpawner
    시간마다 스폰 주기 변경

--- Assets/2.Scripts/Spawn/ZombieSpawner.cs
+++ Assets/2.Scripts/Spawn/ZombieSpawner.cs
@@ -5,6 +5,7 @@ public class ZombieSpawner : MonoBehaviour
 {
     [SerializeField] private Transform _spawnPos;
     private float _spawnInterval = 2f;
+    private WaitForSeconds _wait;
     private void Start()
     {
         GameScene.Instance.StartLoadCallback += StartSpawn;
@@ -13,18 +14,33 @@ public class ZombieSpawner : MonoBehaviour
     private void StartSpawn()
     {
         StartCoroutine(Spawn());
+        StartCoroutine(ChangeSpawnInterval());
     }
 
     private IEnumerator Spawn()
     {
-        var wait = new WaitForSeconds(_spawnInterval);
+        _wait = new WaitForSeconds(_spawnInterval);
         
         while (true)
         {
             ZombieMelee zombie = Managers.Object.Spawn<ZombieMelee>("ZombieMelee.prefab");
             zombie.transform.position = _spawnPos.position; 
 
-            yield return wait;
+            yield return _wait;
         }
+    }
+
+    private IEnumerator ChangeSpawnInterval()
+    {
+        yield return new WaitForSeconds(15f);
+        
+        _spawnInterval = 1f;
+        _wait = new WaitForSeconds(_spawnInterval);
+        
+        yield return new WaitForSeconds(10f);
+
+        _spawnInterval = .5f;
+        _wait = new WaitForSeconds(_spawnInterval);
+
     }
 }
